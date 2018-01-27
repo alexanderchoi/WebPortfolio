3713031ae594d8d00fa6c9e30a1bbda523435722
Alex Choi <achoiweb@gmail.com>
2018-01-27 23:47:54 +0000

    initialize folders

---
+++ dir/app.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+      app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log(`[Server started]`);
+})
