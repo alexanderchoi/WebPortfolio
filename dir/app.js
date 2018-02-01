53239c0b01242efca55129b05287784476917f5a
Alex Choi <achoiweb@gmail.com>
2018-02-01 07:36:14 +0000

    add slide out menu

--- dir/app.js
+++ dir/app.js
@@ -1,16 +1,13 @@
 const express = require('express'),
       app = express();
 
+app.use(express.static('pages'));
 app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res) {
     res.render('index');
 })
-
-
-
-
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
